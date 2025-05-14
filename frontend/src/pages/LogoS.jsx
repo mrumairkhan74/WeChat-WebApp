1fc78fcd5a8dda84ccbae403261f0cf4d297e31e
Umair Khan <125242896+mrumairkhan74@users.noreply.github.com>
2025-05-14 20:30:51 +0500

    Add files via upload

---
+++ frontend/src/pages/LogoS.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const LogoS = () => {
+  return (
+    <div className=' w-full h-screen bg-transparent relative'>
+         <div className="w-[500px] h-[500px] rounded-md bg-transparent backdrop-blur-sm absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 shadow-md ">
+            <img src="./images/signup.png" className='items-center justify-center p-2 w-50 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 animate-spin' />
+         </div>
+    </div>
+  )
+}
+
+export default LogoS
